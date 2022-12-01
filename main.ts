f2aef5b9a51640e0f9503fff5d9445428bc47899
1001dy2 <115143514+1001dy2@users.noreply.github.com>
2022-12-01 16:45:16 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,46 @@
-basic.forever(function () {
-	
-})
+let list = [
+[
+0,
+0,
+0,
+0,
+0
+],
+[
+0,
+1,
+0,
+1,
+0
+],
+[
+0,
+0,
+0,
+0,
+0
+],
+[
+1,
+0,
+0,
+0,
+1
+],
+[
+0,
+1,
+1,
+1,
+0
+]
+]
+for (let y = 0; y <= 4; y++) {
+    for (let index = 0; index <= 4; index++) {
+        if (list[y][index] == 1) {
+            led.plotBrightness(index, y, 255)
+        } else {
+            led.unplot(index, y)
+        }
+    }
+}
